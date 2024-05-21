810af72b7a18b117078519f4ae0ba8947d90e7cc
ninaddande97 <120252029+ninaddande97@users.noreply.github.com>
2024-05-21 19:22:20 +0530

    Add code in main branch

--- demo.java
+++ demo.java
@@ -4,6 +4,7 @@ public class demo{
 public static void main(String[] args) {
     System.out.println("Hello World2");
     System.out.println("New Line");
+    System.out.println("Add new 3 lines");
 }
     
 }
